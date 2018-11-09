11e1dc5e2463f71791bc9419e2720396c97b8d66
TiagoJunior99 <tiagojunior963@outlook.com>
2018-11-09 16:05:21 -0300

    CRUD de produto refatorado

--- SysEstoque/api/controllers/ProductController.js
+++ SysEstoque/api/controllers/ProductController.js
@@ -6,29 +6,20 @@
         const model = req.body.model;
         const serialNumber = req.body.serialNumber;
         
-        const product = {
-            name,  description,  brand, model, serialNumber
-        }
-       
-        if (typeof product === null) {
-           return res.send('Preencha todos os campos!');
-       }
-       console.log('Número de série: ', serialNumber);
         try {
             await Product.create({name, description, brand, model, serialNumber}).fetch();
-            return res.send('Cadastrou o produto');
+            return res.view('pages/Product/list', {products: await Product.find()});
         } catch (error) {
             console.log(error);
-            return res.send('Deu merda');
+            return res.send({error: 'Problemas ao cadastrar o produto'});
         }
     },
     
     list: async function (req, res) {
       try {
-          const products = await Product.find();
-          return res.view('pages/Product/list', {products: products});
+          return res.view('pages/Product/list', {products: await Product.find()});
       } catch (error) {
-          return  res.send('Error ao listar os produtos')
+          return  res.send({error:'Probelmas ao listar os produtos'});
       }  
     },
 
@@ -39,7 +30,7 @@
             return res.view('pages/Product/edit',{product: product});
         } catch (error) {
             console.log(error);
-            return res.send({error:'Error ao procurar um produto.'})
+            return res.send({error:'Problemas ao procurar um produto.'})
         }
     },
 
@@ -49,31 +40,23 @@
         const brand = req.body.brand;
         const model = req.body.model;
         const serialNumber = req.body.serialNumber;
-        const product = {
-            name,  description,  brand, model, serialNumber
-        }
        
-        if (typeof product === null) {
-           return res.send('Preencha todos os campos!');
-       }
         try {
             await Product.update({id: req.params.id},{name, description, brand, model, serialNumber});
-            return res.send('Atualizou o produto');
-
+            return res.view('pages/Product/list', {products: await Product.find()});
         } catch (error) {
             console.log(error);
-            return res.send('Deu merda');
+            return res.send({error: 'Problemas ao atualizar o produto'});
         }
-        return false;
     },
     
     delete: async function(req, res){
         try {
             await Product.destroy({id: req.params.id});
-            return res.send('Excluiu o produto');
+            return res.view('pages/Product/list', {products: await Product.find()});
         } catch (error) {
             console.log(error);
-            return res.send('Deu Merda');
+            return res.send({error: 'Problemas ao deletar um produto'});
 
         }
     },
